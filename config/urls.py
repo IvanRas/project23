72bc6fbf39f0374d02474f5539ec3d586ef2a0ba
root <rastorgu98@gmail.com>
2024-11-05 12:42:45 +0300

    all_finale.0.5

--- config/urls.py
+++ config/urls.py
@@ -24,10 +24,11 @@ from config import settings
 
 app_name = CatalogConfig.name
 
-if settings.DEBUG:
-    urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)[
+urlpatterns = [
         path('admin/', admin.site.urls),
         path("catalog/", include('catalog.urls', namespace="catalog")),
-        path("", include('media.urls', namespace="media")),
-    ]
+]
+
+if settings.DEBUG:
+  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
