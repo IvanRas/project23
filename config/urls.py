a162d81618e99612e1bfbfd867292952ad12f80a
root <rastorgu98@gmail.com>
2024-11-13 13:32:36 +0300

    добавлена ссылка в меню на переход в блог

--- config/urls.py
+++ config/urls.py
@@ -22,7 +22,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('catalog/', include('catalog.urls', namespace='catalog')),
+    path('', include('catalog.urls', namespace='catalog')),
     path('blog/', include('blog.urls', namespace='blog'))
 ]
 
